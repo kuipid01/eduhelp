c3dac25ba101ca743443a19f0e593a47020d0b97
Stephen Busayo Adegoke <106432460+kuipid01@users.noreply.github.com>
2024-02-08 06:09:37 +0000

    homepae

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -5,33 +5,34 @@ import Image from "next/image";
 
 const Homepage = () => {
   return (
-    <section className=" flex justify-center flex-col items-center text-white px-10 bg-brand/prim-blue h-screen">
+    <section className=" flex justify-between flex-col items-center text-white px-10 bg-brand/prim-blue h-screen">
+      <Navbar />{" "}
       <div className="z-10 opacity-20 fixed top-0 left-0 w-full h-screen flex justify-between">
-        {Array.from({ length: 15 }).map((item, i) => (
+        {Array.from({ length: 10 }).map((item, i) => (
           <div className="w-[1px] h-screen bg-neutral-400" key={i}></div>
         ))}
       </div>
       <div className="z-10 fixed top-0 left-0 w-full h-screen flex flex-col justify-between">
-        {Array.from({ length: 15 }).map((item, i) => (
+        {Array.from({ length: 10 }).map((item, i) => (
           <div
             className="w-full h-[1px] opacity-20 bg-neutral-400"
             key={i}
           ></div>
         ))}
       </div>
-      <Navbar />
-      <main className=" z-20 relative w-full  h-[80vh] flex gap-5 flex-col justify-center items-center">
-        <h1 className="sm:max-w-[800px]  leading-[180%] w-4/5 text-3xl font-black text-center">
+      <main className=" z-20 relative w-full  h-[90vh] flex gap-5 flex-col justify-center items-center">
+        {/* <Image src='v' /> */}
+        <h1 className="sm:max-w-[850px]  leading-[180%] w-5/6 text-xl sm:text-4xl font-black text-center">
           Elevate Learning Experiences with Our School Management Solution
         </h1>
-        <p className=" font-light text-neutral-200 w-4/5 sm:max-w-[900px] text-lg text-center ">
+        <p className=" font-light text-neutral-200 w-5/6 sm:max-w-[900px] text-lg text-center ">
           Effortless school management, from attendance tracking to grade
           reporting, all in one place.
         </p>
         <ButtonComp text="🐱‍🏍 Try now !" />
 
         {/* left */}
-        <div className=" absolute bottom-0 left-0 flex  flex-col gap-5">
+        <div className=" absolute bottom-5 left-0 flex  flex-col gap-5">
           <h1 className=" text-xl tracking-wide font-bold uppercase">
             120 + Schools
           </h1>
