d32163a966ca4be581532c99ecbb3b4a6118be97
Stephen Busayo Adegoke <106432460+kuipid01@users.noreply.github.com>
2024-02-09 01:48:07 +0000

    stille changes on extra

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+export const Testimony = () => {
+  <div className="bg-white">
+    <p className="font-light text-base"></p>
+  </div>;
+};
 const Testimonial = () => {
   return (
-    <section>
+    <section className=" h-fit min-h-[80vh] sm:h-screen ">
       <h1 className=" text-2xl text-center">Testimonials</h1>
 
       <div></div>
